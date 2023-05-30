62cdc68a89312c4eb096d735ca4fd044920a83ae
Alexander Todorov <ayagasha@Alexanders-MacBook-Pro.local>
2023-05-30 16:31:32 +0300

    Add constants

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,12 @@ pub const ACTION_DETAILS: &str = "act=4";
 pub const ACTION_LIST: &str = "act=3";
 pub const DATE_FORMAT: &str = "%Y-%m-%d";
 pub const DATE_TIME_FORMAT: &str = "%Y-%m-%d %H:%M:%S";
+pub const NOT_FOUND_MSG: &str = "изтрита или не е активна";
+pub const POWER_TXT: &str = "Мощност";
+pub const GEARBOX_TXT: &str = "Скоростна кутия";
+pub const ENGINE_TXT: &str = "Тип двигател";
+pub const BROWSER_USER_AGENT: &str ="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15";
+
 lazy_static! {
     static ref INIT_LOGGER: Once = Once::new();
 }
