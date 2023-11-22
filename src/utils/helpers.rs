de3456d4337f51dda01d8217b0e8449b4b06dc36
AlexT <e-alexander.todorov@smart.com>
2023-11-22 18:09:23 +0200

    WIP: Removing unused deps from Cargo.toml

--- src/utils/helpers.rs
+++ src/utils/helpers.rs
@@ -106,33 +106,33 @@ pub fn create_empty_csv<T: Serialize + Header>(file_path: &str) -> Result<(), Bo
     Ok(())
 }
 
-pub mod crossbeam_utils {
+// pub mod crossbeam_utils {
 
-    use std::time::Duration;
+//     use std::time::Duration;
 
-    use crossbeam::channel::Receiver;
+//     use crossbeam::channel::Receiver;
 
-    use futures::Stream;
+//     use futures::Stream;
 
-    pub fn to_stream<T>(rx: &mut Receiver<T>) -> impl Stream<Item = T> + '_ {
-        async_stream::stream! {
-            while let Ok(item) = rx.recv()  {
-                yield item;
-            }
-        }
-    }
+//     pub fn to_stream<T>(rx: &mut Receiver<T>) -> impl Stream<Item = T> + '_ {
+//         async_stream::stream! {
+//             while let Ok(item) = rx.recv()  {
+//                 yield item;
+//             }
+//         }
+//     }
 
-    pub fn to_stream_with_timeout<T>(
-        rx: &mut Receiver<T>,
-        seconds: u64,
-    ) -> impl Stream<Item = T> + '_ {
-        async_stream::stream! {
-            while let Ok(item) = rx.recv_timeout(Duration::from_secs(seconds))  {
-                yield item;
-            }
-        }
-    }
-}
+//     pub fn to_stream_with_timeout<T>(
+//         rx: &mut Receiver<T>,
+//         seconds: u64,
+//     ) -> impl Stream<Item = T> + '_ {
+//         async_stream::stream! {
+//             while let Ok(item) = rx.recv_timeout(Duration::from_secs(seconds))  {
+//                 yield item;
+//             }
+//         }
+//     }
+// }
 
 pub mod stream_utils {
 
