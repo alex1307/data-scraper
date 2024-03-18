4677bd2c9d7d222339e4fd254790f4ac8e35daef
Alexander Todorov <e-alexander.todorov@smart.com>
2024-03-18 12:58:22 +0200

    Fix make and model bug with cars.bg and mobile.bg sources

--- src/lib.rs
+++ src/lib.rs
@@ -35,6 +35,18 @@ pub const YEAR_TXT: &str = "Дата на производство";
 
 pub const BROWSER_USER_AGENT: &str ="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15";
 
+pub const SPECIAL_MAKES: [&str; 9] = [
+    "Aston Martin",
+    "Land Rover",
+    "Range Rover",
+    "Ineos Grenadier",
+    "Alfa Romeo",
+    "Rolls-Royce",
+    "Rolls Royce",
+    "Great Wall",
+    "Lynk & Co",
+];
+
 lazy_static! {
     static ref INIT_LOGGER: Once = Once::new();
     pub static ref NOW: DateTime<Utc> = chrono::Utc::now();
